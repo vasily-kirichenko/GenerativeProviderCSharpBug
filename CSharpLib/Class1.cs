6e597507c5ceb4bc032187e7c4c5e135aba73be7
kirichenko <vaskir@gmail.com>
2018-07-25 11:17:56 +0300

    init

---
+++ CSharpLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using LibWithTP;
+
+namespace CSharpLib
+{
+    public static class Class
+    {
+        public static void Foo()
+        {
+            var _ = Config.instance.Level1;
+            var __ = Config.instance.Level1.Level12;
+            var ___ = Config.instance.Level1.Level12.Level13;
+            var ____ = Config.instance.Level2;
+        }
+    }
+}
---
+++ LibWithTP/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace LibWithTP
+
+open FSharp.Configuration
+
+type GeneratedConfig = YamlConfig<YamlText = """
+Level1:
+  Level12:
+    Level13: 1
+Level2:
+  Level21: 2""">
+
+module Config = let instance = GeneratedConfig()
